30b82e8c297d5d149fce856aaa2dabad43a7d245
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2016-12-16 19:59:41 +0330

    Fix any bugs

--- protected/modules/users/views/public/index.php
+++ protected/modules/users/views/public/index.php
@@ -5,8 +5,8 @@
 ?>
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <div class="text-center or-text">
-        <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12 float-none">
+    <div class="text-center">
+        <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12 float-none or-text">
             <a href="<?= $this->createUrl('/googleLogin') ?>" class="btn-red"><i class="google-icon"></i>ورود یا ثبت نام با گوگل</a>
         </div>
     </div>
