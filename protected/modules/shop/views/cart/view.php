8340136b956c1b3374d58bbde9e4913de6b0fccc
Masoud <gharagozlu.masoud@gmail.com>
2017-02-13 19:21:21 +0330

    Shop (frontend)

--- protected/modules/shop/views/cart/view.php
+++ protected/modules/shop/views/cart/view.php
@@ -1,118 +1,82 @@
 <?php
-if($this->id == 'cart')
-	$this->renderPartial('/order/waypoint', array('point' => 0));
-
-if(!isset($books)) 
-	$books = Shop::getCartContent();
-
-if(!isset($this->breadcrumbs) || ($this->breadcrumbs== array()))
-	$this->breadcrumbs = array(
-			Shop::t('Shop') => array('//shop/products/'),
-			Shop::t('Shopping Cart'));
+/* @var $this ShopCartController */
+/* @var $cart Books[] */
+/* @var $model Books */
 ?>
-
-<h2><?php echo Shop::t('Shopping cart'); ?></h2>
-
-
-<?php
-if($books){
-	echo '<table cellpadding="0" cellspacing="0" class="shopping_cart">';
-	printf('<tr><th>%s</th><th>%s</th><th>%s</th><th>%s</th><th style="width:60px;">%s</th><th style="width:60px;">%s</th><th>%s</th></tr>',
-		Shop::t('Image'),
-		Shop::t('Amount'),
-		Shop::t('Product'),
-		Shop::t('Variation'),
-		Shop::t('Price Single'),
-		Shop::t('Sum'),
-		Shop::t('Actions')
-	);
-
-	foreach($books as $position => $book){
-		if(@$model = Books::model()->findByPk($book['book_id'])){
-
-			// render row of basket table
-			printf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td class="text-right">%s</td><td class="text-right price_' . $position . '">%s</td><td>%s</td></tr>',
-				$model->getImage(0, true),
-				CHtml::textField('amount_' . $position,
-					$book['amount'], array(
-						'size' => 4,
-						'class' => 'amount_' . $position,
-					)
-				),
-				$model->title,
-				$variations,
-				Shop::priceFormat($model->getPrice(@$book['Variations'])),
-				Shop::priceFormat($model->getPrice(@$book['Variations'], @$book['amount'])),
-				CHtml::link(Shop::t('Remove'), array(
-					'//shop/shoppingCart/delete',
-					'id' => $position), array(
-					'confirm' => Shop::t('Are you sure?')))
-			);
-
-			Yii::app()->clientScript->registerScript('amount_' . $position, "
-					$('.amount_" . $position . "').keyup(function() {
-						$.ajax({
-							url:'" . $this->createUrl('//shop/shoppingCart/updateAmount') . "',
-							data: $('#amount_" . $position . "'),
-							success: function(result) {
-							$('.amount_" . $position . "').css('background-color', 'lightgreen');
-							$('.widget_amount_" . $position . "').css('background-color', 'lightgreen');
-							$('.widget_amount_" . $position . "').html($('.amount_" . $position . "').val());
-							$('.price_" . $position . "').html(result);	
-							$('.price_total').load('" . $this->createUrl(
-					'//shop/shoppingCart/getPriceTotal') . "');
-							},
-							error: function() {
-							$('#amount_" . $position . "').css('background-color', 'red');
-							},
-
-							});
-				});
-			");
-			// end of render row
-		}
-	}
-
-	// نمایش روش های تحویل
-	if($shippingMethod = Shop::getShippingMethod()){
-		printf('<tr>
-				<td></td>
-				<td>1</td>
-				<td>%s</td>
-				<td></td>
-				<td class="text-right">%s</td>
-				<td class="text-right">%s</td>
-				<td>%s</td></tr>',
-			Shop::t('Shipping costs'),
-			Shop::priceFormat($shippingMethod->price),
-			Shop::priceFormat($shippingMethod->price),
-			CHtml::link(Shop::t('edit'), array('//shop/shippingMethod/choose'))
-		);
-	}
-	echo '<tr>
-			<td class="text-right no-border" colspan="6">
-			<p class="price_total">' . Shop::getPriceTotal() . '</p></td>
-			<td class="no-border"></td></tr>';
-	echo '</table>';
-	?>
-
-	<?php
-	if(Yii::app()->controller->id != 'order'){
-		echo '<div class="buttons">';
-		echo CHtml::link(Shop::t('Buy additional Products'), array(
-			'//shop/products'), array('class' => 'btn-previous'));
-
-		echo '<br />';
-
-		echo CHtml::link(Shop::t('Buy this products'), array(
-			'//shop/order/create'), array('class' => 'btn-next'));
-		echo '</div>';
-	}
-
-	?>
-	<div class="clear"></div>
-
-	<?php
-
-} else echo Shop::t('Your shopping cart is empty'); ?>
-
+<div class="page">
+	<div class="page-heading">
+		<div class="container">
+			<h1>سبد خرید شما</h1>
+		</div>
+	</div>
+	<div class="container page-content">
+		<div class="white-box cart">
+			<table class="table table-striped">
+				<thead>
+				<tr>
+					<th>شرح محصول</th>
+					<th class="text-center">تعداد</th>
+					<th class="text-center hidden-xs">قیمت واحد</th>
+					<th class="text-center hidden-xs">قیمت کل</th>
+					<th class="hidden-xs"></th>
+				</tr>
+				</thead>
+				<tbody>
+                <?php foreach($books as $position => $book):?>
+                    <?php if(@$model = Books::model()->findByPk($book['book_id'])):?>
+                        <tr>
+                            <td>
+                                <img src="<?php echo Yii::app()->baseUrl."/uploads/books/icons/".$model->icon;?>" alt="<?php echo CHtml::encode($model->title);?>" class="hidden-xs hidden-sm">
+                                <div class="info">
+                                    <h4><?php echo CHtml::encode($model->title);?></h4>
+                                    <span class="item hidden-xs">نویسنده: <span class="value"><?php echo CHtml::encode($model->getPerson("نویسنده"));?></span></span>
+                                    <span class="item hidden-xs">ناشر: <span class="value"><?php echo CHtml::encode($model->getPublisherName());?></span></span>
+                                    <span class="item hidden-xs">سال چاپ: <span class="value"><?php echo CHtml::encode($model->lastPackage->print_year);?></span></span>
+                                    <span class="item hidden-xs">تعداد صفحات: <span class="value"><?php echo CHtml::encode($model->number_of_pages);?> صفحه</span></span>
+                                </div>
+                            </td>
+                            <td class="vertical-middle text-center">
+                                <?php echo CHtml::dropDownList('qty_'.$position, $book["qty"], array(1,2,3,4,5,6,7,8,9,10), array("class"=>"quantity"));?>
+                                <?php CHtml::link("حذف", array('//shop/cart/remove', 'id' => $position), array("class"=>"remove", 'confirm' => Shop::t('Are you sure?')));?>
+                            </td>
+                            <td class="vertical-middle text-center hidden-xs">
+                                <span class="price"><?php echo Controller::parseNumbers(number_format($model->lastPackage->price))?><small> تومان</small></span>
+                            </td>
+                            <td class="vertical-middle text-center hidden-xs">
+                                <span class="price"><?php echo Controller::parseNumbers(number_format($book["qty"]*$model->lastPackage->price))?><small> تومان</small></span>
+                            </td>
+                            <td class="vertical-middle text-center hidden-xs">
+                                <?php CHtml::link("حذف", array('//shop/cart/remove', 'id' => $position), array("class"=>"remove", 'confirm' => Shop::t('Are you sure?')));?>
+                            </td>
+                        </tr>
+                    <?php endif;?>
+                <?php endforeach;?>
+				</tbody>
+			</table>
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 pull-left total-container">
+				<div class="row">
+                    <?php $cartStatistics=Shop::getPriceTotal();?>
+					<ul class="list-group green-list">
+						<li class="list-group-item">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">جمع کل خرید شما</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 price text-center"><?php echo Controller::parseNumbers(number_format($cartStatistics["totalPrice"]));?><small> تومان</small></div>
+						</li>
+						<li class="list-group-item">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 tax-container">مالیات بر ارزش افزوده<small> (9 درصد)</small></div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 price text-center"><?php echo Controller::parseNumbers(number_format($cartStatistics["totalTax"]));?><small> تومان</small></div>
+						</li>
+						<li class="list-group-item">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 total">مبلغ قابل پرداخت</div>
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 price text-center total-value"><?php echo Controller::parseNumbers(number_format($cartStatistics["totalPayment"]));?><small> تومان</small></div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="buttons">
+				<input type="submit" class="btn-black pull-right" value="بازگشت به صفحه اصلی">
+				<input type="submit" class="btn-blue pull-left" value="انتخاب شیوه ارسال">
+			</div>
+		</div>
+	</div>
+</div>
