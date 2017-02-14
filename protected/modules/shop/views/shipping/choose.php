72bcb9845c88a0728c1243a787f327c18bf34f64
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2017-02-14 16:36:18 +0330

    Start Shop Actions

--- protected/modules/shop/views/shipping/choose.php
+++ protected/modules/shop/views/shipping/choose.php
@@ -48,7 +48,7 @@ Yii::app()->clientScript->registerScript('delete-update-address-script','
         </div>
     </div>
     <div class="container page-content relative">
-        <?php $this->renderPartial('//partial-views/_loading', array('id' => 'basket-loading')) ?>
+        <?php $this->renderPartial('shop.views.shipping._loading', array('id' => 'basket-loading')) ?>
         <div class="white-box cart">
             <?php $this->renderPartial('/order/_steps', array('point' => 1));?>
             <div class="select-address">
