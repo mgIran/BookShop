d40e7975f3480b67ae6f9235bbe37844b2b50b1b
YusefMobasheri <yusef.mobasheri@gmail.com>
2019-02-04 21:45:02 +0330

    Delete any files

--- protected/components/sms-ir/DeleteSchedulSendSms.php
+++
@@ -1,6 +0,0 @@
-<?php
-$client=new SoapClient('http://n.sms.ir/ws/SendReceive.asmx?wsdl');
-$params= array('userName'=>test,'password'=>test,'schedulSendSmsID'=>'1');
-print_r($client->DeleteSchedulSendSms($params));
-echo $client->DeleteSchedulSendSms($params)->message;
-?>
--- protected/components/sms-ir/GetSmsLines.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$client = new SoapClient("http://n.sms.ir/ws/SendReceive.asmx?wsdl");
-$params= array("userName"=>test,"password"=>test));
-print_r( $client->GetSmsLines($params);  
-echo $client->GetSmsLines($params)->message;
-?>
